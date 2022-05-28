57107e93bb2792d2981112c8ef08053ae0489de2
algovitae <algovitae@zohomail.com>
2022-05-28 08:35:51 +0000

    nexus, apollo

---
+++ src/schema.ts
@@ -0,0 +1,103 @@
+import { interfaceType, makeSchema, objectType, queryType } from "nexus";
+import { join } from "path";
+
+// TODO: relationなどを明確にする
+// TODO: PK,SKの組み合わせの場合にどうするべきか考える
+
+// const Node = interfaceType({
+//     name: 'Node',
+//     definition(t) {
+//         t.id('id', { description: 'unique identifier for the resource' })
+//     },
+// })
+
+// const User = objectType({
+//     name: 'User',
+//     isTypeOf(source) {
+//         return 'user_name' in source
+//     },
+//     definition(t) {
+//         t.implements(Node)
+//         t.string('user_name')
+//     }
+// })
+
+// const UserIdentity = objectType({
+//     name: "UserIdentity",
+//     isTypeOf(source) {
+//         return 'email' in source
+//     },
+//     definition(t) {
+//         t.implements(Node)
+//         t.string('user_id')
+//         t.string('email')
+//         t.string('password_hash')
+//     }
+// })
+
+// const Group = objectType({
+//     name: "Group",
+//     isTypeOf(source) {
+//         return 'group_name' in source
+//     },
+//     definition(t) {
+//         t.implements(Node)
+//         t.string("group_name")
+//     }
+// })
+
+// const Membership = objectType({
+//     name: "Membership",
+//     isTypeOf(source) {
+//         return 'user_id' in source && 'group_id' in source
+//     },
+//     definition(t) {
+//         t.implements(Node)
+//         t.string("group_id")
+//         t.string("user_id")
+//     }
+// })
+
+
+// const Thread = objectType({
+//     name: "Thread",
+//     isTypeOf(source) {
+//         return 'theread_name' in source
+//     },
+//     definition(t) {
+//         t.implements(Node)
+//         t.string("group_id")
+//         t.string("thread_id")
+//         t.string("thread_name")
+//     }
+// })
+
+// const ThreadComment = objectType({
+//     name: "ThreadComment",
+//     isTypeOf(source) {
+//         return 'comment_id' in source
+//     },
+//     definition(t) {
+//         t.implements(Node)
+//         t.string("thread_id")
+//         t.string("comment_id")
+//         t.string("title")
+//         t.string("body")
+//         t.string("comment_by")
+//     }
+// })
+
+export const schema = makeSchema({
+    types: {
+        // User,
+        // UserIdentity,
+        // Group,
+        // Membership,
+        // Thread,
+        // ThreadComment
+    },
+  outputs: {
+    typegen: join(__dirname, '..', 'nexus-typegen.ts'),
+    schema: join(__dirname, '..', 'schema.graphql'),
+  },
+})
