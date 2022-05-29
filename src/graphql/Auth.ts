bd892cde9a2aff2c52724d3f9566c8eb98bd89cd
algovitae <algovitae@zohomail.com>
2022-05-29 02:01:33 +0000

    implemented login mutation

--- src/graphql/Auth.ts
+++ src/graphql/Auth.ts
@@ -1,31 +1,40 @@
+import { compare } from "bcrypt"
 import { mutationType, objectType, stringArg } from "nexus"
+import { userIdentityStore } from "../ddb/UserIdentity"
 import { UserIdentity } from "./UserIdentity"
 
 export const Auth = objectType({
     name: "Auth",
     definition(t) {
         t.string("token"),
-        t.field("user_identity", {
-            type: UserIdentity,
-        })
+            t.field("user_identity", {
+                type: UserIdentity,
+            })
     }
 })
 
 export const AuthMutation = mutationType({
     definition(t) {
-      t.field("login", {
-        type: Auth,
-        args: {
-            email: stringArg(),
-            password: stringArg(),
-        },
-        resolve(source, args, context) {
-            if (args.password === 'password') {
-                return { token: 'the token', user_identity: { user_id: 'UserIdentity/8fe790de-dee7-11ec-94cf-0242ac140002' }}
-            } else {
-                return null;
+        t.field("login", {
+            type: Auth,
+            args: {
+                email: stringArg(),
+                password: stringArg(),
+            },
+            async resolve(source, args, context) {
+                const userIdentity = await userIdentityStore.get(args.email).exec()
+                if (!userIdentity) {
+                    return null
+                }
+                const compared = await compare(args.password ?? "", userIdentity.password_hash)
+                if (!compared) {
+                    return null
+                }
+                return {
+                    token: 'the token',
+                    user_identity: userIdentity
+                }
             }
-        }
-      })
+        })
     }
-  })
+})
